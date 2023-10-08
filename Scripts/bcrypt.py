2bd9e7152180df47bd2bed0d6a982de54d3abda1
KafetzisThomas <tomkafetzis06@gmail.com>
2023-10-08 21:40:51 +0300

    Move list to new file

--- Scripts/bcrypt.py
+++ Scripts/bcrypt.py
@@ -1,14 +1,5 @@
 import bcrypt
-
-chars = [
-    'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k',
-    'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
-    'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6',
-    '7', '8', '9', '!', '"', '#', '$', '%', '&', "'", '*',
-    '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?',
-    '@', '^', '_', '`', '{', '|', '}', '~', '[', '\\', ']',
-    '(', ')'
-]
+from Scripts.chars import chars
 
 def crack_bcrypt_hash(hash_str, password_length=1):
     """Find & return a matching password for a given bcrypt hash"""
