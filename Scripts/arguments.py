90ef20561bc749e3eb25066da5c3236b76283a8e
KafetzisThomas <tomkafetzis06@gmail.com>
2024-03-24 22:28:41 +0200

    Refactor code into a function

---
+++ Scripts/arguments.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+# -*- coding: UTF-8 -*-
+
+import sys, time
+
+from Scripts.hash_algorithms.bcrypt import crack_bcrypt_hash
+from Scripts.hash_algorithms.md5 import crack_md5_hash
+from Scripts.hash_algorithms.sha1 import crack_sha1_hash
+from Scripts.hash_algorithms.sha224 import crack_sha224_hash
+from Scripts.hash_algorithms.sha256 import crack_sha256_hash
+from Scripts.hash_algorithms.sha384 import crack_sha384_hash
+from Scripts.hash_algorithms.sha512 import crack_sha512_hash
+from colorama import Fore as F, Back as B
+
+def command_line_arguments(hash_algorithm):
+    hash_algorithms = {
+        "-bcrypt": crack_bcrypt_hash,
+        "-md5": crack_md5_hash,
+        "-sha1": crack_sha1_hash,
+        "-sha224": crack_sha224_hash,
+        "-sha256": crack_sha256_hash,
+        "-sha384": crack_sha384_hash,
+        "-sha512": crack_sha512_hash
+    }
+
+    try:
+        if sys.argv[1] == hash_algorithm:
+            hash_str = sys.argv[2]
+            wordlist = None
+            with_wordlist = False
+            if "--with-wordlist" in sys.argv:
+                wordlist = sys.argv[3]
+                with_wordlist = True
+            start = time.time()
+            plain_text = hash_algorithms[hash_algorithm](hash_str=hash_str, wordlist=wordlist, with_wordlist=with_wordlist)
+            end = time.time()
+            time_elapsed = end - start
+            if plain_text:
+                print(f"Password Found: {B.LIGHTRED_EX}{F.BLACK} {plain_text} {F.RESET}{B.RESET}")
+                print(f"Time elapsed: {time_elapsed:.1f}s")
+            sys.exit()
+    except IndexError:
+        pass
--- main.py
+++ main.py
@@ -7,18 +7,12 @@
 # NOTE: By contributing to this project, you agree to the terms of the GPLv3 license, and agree to grant the project owner the right to also provide or sell this software, including your contribution, to anyone under any other license, with no compensation to you.
 
 # Import built-in modules
-import platform, os, sys, time
+import platform, os, sys
 
 # Import module files
-from Scripts.hash_algorithms.bcrypt import crack_bcrypt_hash
-from Scripts.hash_algorithms.md5 import crack_md5_hash
-from Scripts.hash_algorithms.sha1 import crack_sha1_hash
-from Scripts.hash_algorithms.sha224 import crack_sha224_hash
-from Scripts.hash_algorithms.sha256 import crack_sha256_hash
-from Scripts.hash_algorithms.sha384 import crack_sha384_hash
-from Scripts.hash_algorithms.sha512 import crack_sha512_hash
 from Scripts.utils import get_user_input
 from Scripts.utils import get_hash_input_and_crack
+from Scripts.arguments import command_line_arguments
 
 # Import other (third-party) modules
 import colorama
@@ -35,116 +29,14 @@ if sys.version_info[0] < 3 or sys.version_info[1] < 6:
     print("Error Code U-2: This script requires running python 3.6 or higher! You are running" + str(sys.version_info[0]) + "." + str(sys.version_info[1]))
     sys.exit()
 
-try:
-    if sys.argv[1] == "-bcrypt":
-        hash_str = sys.argv[2]
-        wordlist = None
-        with_wordlist = False
-        if "--with-wordlist" in sys.argv:
-            wordlist = sys.argv[3]
-            with_wordlist = True
-        start = time.time()
-        plain_text = crack_bcrypt_hash(hash_str=hash_str, wordlist=wordlist, with_wordlist=with_wordlist)
-        end = time.time()
-        time_elapsed = end - start
-        if plain_text:
-            print(f"Password Found: {B.LIGHTRED_EX}{F.BLACK} {plain_text} ")
-            print(f"Time elapsed: {time_elapsed:.1f}s")
-        sys.exit()
-    elif sys.argv[1] == "-md5":
-        hash_str = sys.argv[2]
-        wordlist = None
-        with_wordlist = False
-        if "--with-wordlist" in sys.argv:
-            wordlist = sys.argv[3]
-            with_wordlist = True
-        start = time.time()
-        plain_text = crack_md5_hash(hash_str=hash_str, wordlist=wordlist, with_wordlist=with_wordlist)
-        end = time.time()
-        time_elapsed = end - start
-        if plain_text:
-            print(f"Password Found: {B.LIGHTRED_EX}{F.BLACK} {plain_text} ")
-            print(f"Time elapsed: {time_elapsed:.1f}s")
-        sys.exit()
-    elif sys.argv[1] == "-sha1":
-        hash_str = sys.argv[2]
-        wordlist = None
-        with_wordlist = False
-        if "--with-wordlist" in sys.argv:
-            wordlist = sys.argv[3]
-            with_wordlist = True
-        start = time.time()
-        plain_text = crack_sha1_hash(hash_str=hash_str, wordlist=wordlist, with_wordlist=with_wordlist)
-        end = time.time()
-        time_elapsed = end - start
-        if plain_text:
-            print(f"Password Found: {B.LIGHTRED_EX}{F.BLACK} {plain_text} ")
-            print(f"Time elapsed: {time_elapsed:.1f}s")
-        sys.exit()
-    elif sys.argv[1] == "-sha224":
-        hash_str = sys.argv[2]
-        wordlist = None
-        with_wordlist = False
-        if "--with-wordlist" in sys.argv:
-            wordlist = sys.argv[3]
-            with_wordlist = True
-        start = time.time()
-        plain_text = crack_sha224_hash(hash_str=hash_str, wordlist=wordlist, with_wordlist=with_wordlist)
-        end = time.time()
-        time_elapsed = end - start
-        if plain_text:
-            print(f"Password Found: {B.LIGHTRED_EX}{F.BLACK} {plain_text} ")
-            print(f"Time elapsed: {time_elapsed:.1f}s")
-        sys.exit()
-    elif sys.argv[1] == "-sha256":
-        hash_str = sys.argv[2]
-        wordlist = None
-        with_wordlist = False
-        if "--with-wordlist" in sys.argv:
-            wordlist = sys.argv[3]
-            with_wordlist = True
-        start = time.time()
-        plain_text = crack_sha256_hash(hash_str=hash_str, wordlist=wordlist, with_wordlist=with_wordlist)
-        end = time.time()
-        time_elapsed = end - start
-        if plain_text:
-            print(f"Password Found: {B.LIGHTRED_EX}{F.BLACK} {plain_text} ")
-            print(f"Time elapsed: {time_elapsed:.1f}s")
-        sys.exit()
-    elif sys.argv[1] == "-sha384":
-        hash_str = sys.argv[2]
-        wordlist = None
-        with_wordlist = False
-        if "--with-wordlist" in sys.argv:
-            wordlist = sys.argv[3]
-            with_wordlist = True
-        start = time.time()
-        plain_text = crack_sha384_hash(hash_str=hash_str, wordlist=wordlist, with_wordlist=with_wordlist)
-        end = time.time()
-        time_elapsed = end - start
-        if plain_text:
-            print(f"Password Found: {B.LIGHTRED_EX}{F.BLACK} {plain_text} ")
-            print(f"Time elapsed: {time_elapsed:.1f}s")
-        sys.exit()
-    elif sys.argv[1] == "-sha512":
-        hash_str = sys.argv[2]
-        wordlist = None
-        with_wordlist = False
-        if "--with-wordlist" in sys.argv:
-            wordlist = sys.argv[3]
-            with_wordlist = True
-        start = time.time()
-        plain_text = crack_sha512_hash(hash_str=hash_str, wordlist=wordlist, with_wordlist=with_wordlist)
-        end = time.time()
-        time_elapsed = end - start
-        if plain_text:
-            print(f"Password Found: {B.LIGHTRED_EX}{F.BLACK} {plain_text} ")
-            print(f"Time elapsed: {time_elapsed:.1f}s")
-        sys.exit()
-except IndexError:
-    if len(sys.argv) > 1:
-        print("Incorrect usage of command line arguments.")
-        sys.exit()
+# Each call attempts to crack a hash specified via command line arguments
+command_line_arguments("-bcrypt")
+command_line_arguments("-md5")
+command_line_arguments("-sha1")
+command_line_arguments("-sha224")
+command_line_arguments("-sha256")
+command_line_arguments("-sha384")
+command_line_arguments("-sha512")
 
 print(text2art("Hash-Breaker"))
 print(f"> Author: {F.LIGHTYELLOW_EX}KafetzisThomas")
