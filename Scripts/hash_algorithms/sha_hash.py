6b89b9a70d8d85956541b928429c00b50de5ece7
KafetzisThomas <tomkafetzis06@gmail.com>
2024-06-15 19:10:41 +0300

    Add arg for custom charset in dynamic generation

--- Scripts/hash_algorithms/sha_hash.py
+++ Scripts/hash_algorithms/sha_hash.py
@@ -6,14 +6,22 @@ from Scripts.gen_pass import generate_passwords
 
 
 def crack_sha_hash(
-    hash_type, hash_str, wordlist=None, password_length=1, with_wordlist=False
+    hash_type,
+    hash_str,
+    wordlist,
+    custom_charset,
+    password_length=1,
+    with_wordlist=False,
+    with_custom_charset=False,
 ):
     """Find & return a matching password for a given sha type of hash (sha1, sha224, etc.)."""
     hash_func = getattr(hashlib, hash_type)
 
     if not with_wordlist:
         while True:
-            for guess in generate_passwords(password_length):
+            for guess in generate_passwords(
+                password_length, custom_charset, with_custom_charset
+            ):
                 if hash_func(guess.encode("utf-8")).hexdigest() == hash_str:
                     return guess
             password_length += 1  # If no match is found, increase the password length
