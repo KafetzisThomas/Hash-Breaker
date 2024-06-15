b7beae42a83d29d3d076b58360a7f1d45d4528bd
KafetzisThomas <tomkafetzis06@gmail.com>
2024-06-15 13:34:10 +0300

    Remove menu - use only command-line args

--- main.py
+++ main.py
@@ -6,53 +6,89 @@
 # License: GPLv3
 # NOTE: By contributing to this project, you agree to the terms of the GPLv3 license, and agree to grant the project owner the right to also provide or sell this software, including your contribution, to anyone under any other license, with no compensation to you.
 
-# Import built-in modules
-import platform, os, sys
-
-# Import module files
-from Scripts.utils import get_user_input
-from Scripts.utils import get_hash_input_and_crack
-from Scripts.arguments import command_line_arguments
-
-# Import other (third-party) modules
+import sys
+import time
 import colorama
-from art import text2art
 from colorama import Fore as F, Back as B
+from Scripts.hash_algorithms.bcrypt import crack_bcrypt_hash
+from Scripts.hash_algorithms.md5 import crack_md5_hash
+from Scripts.hash_algorithms.sha1 import crack_sha1_hash
+from Scripts.hash_algorithms.sha224 import crack_sha224_hash
+from Scripts.hash_algorithms.sha256 import crack_sha256_hash
+from Scripts.hash_algorithms.sha384 import crack_sha384_hash
+from Scripts.hash_algorithms.sha512 import crack_sha512_hash
+
+# Initialize colorama for colored output
 colorama.init(autoreset=True)
 
-# Check system platform to set correct console clear command
-clear_command = "cls" if platform.system() == "Windows" else "clear"
-os.system(clear_command)  # Clear console
+# Dictionary mapping hash algorithm names to their respective functions
+hash_algorithms = {
+    "bcrypt": crack_bcrypt_hash,
+    "md5": crack_md5_hash,
+    "sha1": crack_sha1_hash,
+    "sha224": crack_sha224_hash,
+    "sha256": crack_sha256_hash,
+    "sha384": crack_sha384_hash,
+    "sha512": crack_sha512_hash,
+}
 
-# Run check on python version, must be 3.6 or higher because of f strings
-if sys.version_info[0] < 3 or sys.version_info[1] < 6:
-    print("Error Code U-2: This script requires running python 3.6 or higher! You are running" + str(sys.version_info[0]) + "." + str(sys.version_info[1]))
-    sys.exit()
 
-# Each call attempts to crack a hash specified via command line arguments
-command_line_arguments("bcrypt")
-command_line_arguments("md5")
-command_line_arguments("sha1")
-command_line_arguments("sha224")
-command_line_arguments("sha256")
-command_line_arguments("sha384")
-command_line_arguments("sha512")
+def crack_hash(hash_algorithm, hash_str, wordlist, with_wordlist):
+    """Crack a given hash using the specified algorithm."""
+    if hash_algorithm in hash_algorithms:
+        start_time = time.time()
+        plain_text = hash_algorithms[hash_algorithm](
+            hash_str=hash_str,
+            wordlist=wordlist,
+            with_wordlist=with_wordlist,
+        )
+        end_time = time.time()
+        time_elapsed = end_time - start_time
 
-print(text2art("Hash-Breaker"))
-print(f"> Author: {F.LIGHTYELLOW_EX}KafetzisThomas")
-print("-------------------------")
-print(f"* You can enter {F.LIGHTMAGENTA_EX}Ctrl+C{F.RESET} to stop the process.")
-print("\nMenu:")
-print(f"  |- 1) Bcrypt  ({F.LIGHTGREEN_EX}STRONG{F.RESET})")
-print(f"  |- 2) MD5     ({F.LIGHTRED_EX}WEAK{F.RESET})")
-print(f"  |- 3) SHA-1   ({F.LIGHTYELLOW_EX}MODERATE{F.RESET})")
-print(f"  |- 4) SHA-224 ({F.LIGHTYELLOW_EX}MODERATE{F.RESET})")
-print(f"  |- 5) SHA-256 ({F.LIGHTYELLOW_EX}MODERATE{F.RESET})")
-print(f"  |- 6) SHA-384 ({F.LIGHTYELLOW_EX}MODERATE{F.RESET})")
-print(f"  |- 7) SHA-512 ({F.LIGHTYELLOW_EX}MODERATE{F.RESET})")
+        if plain_text:
+            print(
+                f"Password Found: {B.LIGHTRED_EX}{F.BLACK} {plain_text} {F.RESET}{B.RESET}"
+            )
+            print(f"Time elapsed: {time_elapsed:.1f}s")
+        else:
+            print("Password not found.")
+    else:
+        print(f"[*] Unsupported hash algorithm: {hash_algorithm}")
 
-choice, with_wordlist, wordlist = get_user_input()
-plain_text, time_elapsed = get_hash_input_and_crack(choice, with_wordlist, wordlist)
 
-print(f"Password Found: {B.LIGHTRED_EX}{F.BLACK} {plain_text} ")
-print(f"Time elapsed: {time_elapsed:.1f}s")
+def commandline_args():
+    """Handle command-line arguments to crack hashes."""
+    if 0 < len(sys.argv) < 6:
+        hash_algorithm = sys.argv[1]
+        hash_str = sys.argv[2]
+        wordlist = None
+        with_wordlist = False
+
+        if "--with-wordlist" in sys.argv:
+            wordlist = sys.argv[3]
+            with_wordlist = True
+
+        crack_hash(hash_algorithm, hash_str, wordlist, with_wordlist)
+    else:
+        print(
+            "[*] Usage: python3 main.py <hash_algo> '<hash_to_crack>' ['<wordlist_path>' --with-wordlist]"
+        )
+
+
+if __name__ == "__main__":
+    # Check Python version requirement
+    if sys.version_info[:2] < (3, 6):
+        print(
+            "Error Code U-2: This script requires Python 3.6 or higher! You are running {}.{}".format(
+                sys.version_info[0], sys.version_info[1]
+            )
+        )
+        sys.exit()
+
+    try:
+        commandline_args()
+    except IndexError:
+        print(
+            "[*] Usage: python3 main.py <hash_algo> '<hash_to_crack>' ['<wordlist_path>' --with-wordlist]"
+        )
+        sys.exit()
