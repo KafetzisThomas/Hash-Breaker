b2349c08929e8ba47ee4deeb723b634e5021ea14
KafetzisThomas <tomkafetzis06@gmail.com>
2024-06-15 10:24:21 +0300

    Add validation for wordlist file existence

--- Scripts/hash_algorithms/bcrypt.py
+++ Scripts/hash_algorithms/bcrypt.py
@@ -4,18 +4,24 @@
 import bcrypt
 from Scripts.gen_pass import generate_passwords
 
+
 def crack_bcrypt_hash(hash_str, wordlist, password_length=1, with_wordlist=False):
     """Find & return a matching password for a given bcrypt hash"""
     if not with_wordlist:
         while True:
             for guess in generate_passwords(password_length):
                 # Check if the generated password matches the hash
-                if bcrypt.checkpw(guess.encode('utf-8'), hash_str.encode('utf-8')):
+                if bcrypt.checkpw(guess.encode("utf-8"), hash_str.encode("utf-8")):
                     return guess
 
             password_length += 1  # If no match is found, increase the password length
     else:
-        with open(wordlist, 'r') as file:
-            for line in file.readlines():
-                if bcrypt.checkpw(line.strip().encode('utf-8'), hash_str.encode('utf-8')):
-                    return line.strip()
+        try:
+            with open(wordlist, "r") as file:
+                for line in file.readlines():
+                    if bcrypt.checkpw(
+                        line.strip().encode("utf-8"), hash_str.encode("utf-8")
+                    ):
+                        return line.strip()
+        except FileNotFoundError:
+            print(f"[!] Wordlist file not found: {wordlist}")
